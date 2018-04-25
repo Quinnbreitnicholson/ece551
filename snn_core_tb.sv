c5b25e63a262f0ada75b46799a3d5cefea79ff1e
Quinnbreitnicholson <quinnbreitnicholson@gmail.com>
2018-04-25 11:29:56 -0500

    Add files via upload

--- snn_core_tb.sv
+++ snn_core_tb.sv
@@ -1,36 +1,21 @@
 module snn_core_tb();
-reg clk, rst_n;
-
-initial begin
-	clk = 0;
-	forever
-		#5 clk = ~clk;
-end
-
-reg start;
+reg start, q_input, clk, data ,we;
+reg[9:0] addr_input_unit, addr;
+reg[3:0] digit;
 wire done, q;
-wire [3:0] digit;
-wire [9:0] addr_input;
-snn_core iDUT(.start(start), .q_input(q), .addr_input_unit(addr_input), .digit(digit), .done(done), .clk(clk), .rst_n(rst_n));
-
-
-ram #(.DATA_WIDTH(1), .ADDR_WIDTH(10), .FILE_IN("sample_in/ram_input_contents_sample_0.txt"))
-	input_unit(.data(1'b0), .addr(addr_input),
-				.we(1'b0), .q(q), .clk(clk));
-
+snn_core core(.start(start), .q_input(q_input), .addr_input_unit(addr_input_unit),
+		 .digit(digit), .clk(clk));
 initial begin
-	rst_n = 0;
-	start = 0;
-	#10;
-	rst_n = 1;
-	start = 1;
-	#10
-	start = 0;
-	@(posedge done)
-	;
-	$display("digit: %h", digit);
-	$stop();
+clk = 0;
+	forever #1 clk = ~clk;
 end
-
+ram#(.ADDR_WIDTH(10), .DATA_WIDTH(1), .FILE_IN("ram_input_contents_sample_0.txt"))
+	ram_test(.data(data), .addr(addr_input_unit), .we(we), .clk(clk), .q(q_input));
+initial begin 
+	start = 1;
+	while(done == 0) 
+		begin
+		#2;
+		end
+end
 endmodule
-
